3f5076c2cc19dcca510cc08d6189727a655a2307
dithyrambe <dithyrambe@outlook.fr>
2024-07-29 23:24:11 +0200

    :tada: Setup tlk

---
+++ src/tlk/cli/main.py
@@ -0,0 +1,26 @@
+from rich.console import Console
+from typer import Typer
+import typer
+
+from tlk.constants import PACKAGE_NAME
+
+
+app = Typer(add_completion=False)
+
+
+def show_version(flag: bool):
+    if flag:
+        from importlib.metadata import version
+        
+        console = Console()
+        console.print(f"release [bold]{version(PACKAGE_NAME)}[bold]")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    version: bool = typer.Option(
+        None, "--version", callback=show_version, help="Show version."
+    ),
+):
+    """Make release"""
